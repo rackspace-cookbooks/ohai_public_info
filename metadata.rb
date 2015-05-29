e9e6271fd24fc45ab5a93d6538cd12ad31a9ee2d
theborch <theborch@gmail.com>
2015-05-29 11:38:15 -0500

    bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description      'Installs/Configures ohai_public_info.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url      'https://github.com/rackspace-cookbooks/ohai_public_info'
 issues_url       'https://github.com/rackspace-cookbooks/ohai_public_info/issues'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'ohai'
 depends 'poise'
